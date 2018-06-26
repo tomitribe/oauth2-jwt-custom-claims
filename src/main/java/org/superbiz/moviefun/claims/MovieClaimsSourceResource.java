4ef93e0465db05791523da4b485770174f5e28e3
Jean-Louis Monteiro <jeanouii@gmail.com>
2018-06-26 19:19:40 +0200

    Small tweaks while doing a dry run

--- src/main/java/org/superbiz/moviefun/claims/MovieClaimsSourceResource.java
+++ src/main/java/org/superbiz/moviefun/claims/MovieClaimsSourceResource.java
@@ -14,15 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.superbiz.moviefun.sts;
+package org.superbiz.moviefun.claims;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
 import java.util.Map;
 
 @Path("claims")
